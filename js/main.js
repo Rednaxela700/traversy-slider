fe7bd01725a2bd26f97d099b9030667c0fc8a905
rednaxela700 <ohleg@windowslive.com>
2019-07-24 00:23:14 +0200

    js 5 ready, can change now to ES6

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
 /**
  * Created by alexander on 23/07/2019.
  */
+var i = 0; // Start point
+var images = [];
+var time = 3000;
+
+//Image list
+images[0] = 'img/img-1.jpg';
+images[1] = 'img/img-2.jpg';
+images[2] = 'img/img-3.jpg';
+
+//Change Image
+function changeImg() {
+    document.slide.src = images[i];
+    if (i < images.length - 1) {
+        i++;
+    } else  {
+        i = 0;
+
+    }
+
+    setTimeout("changeImg()", time);
+
+}
+
+window.onload = changeImg();
